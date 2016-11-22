a618288afc7c420e4f58119c13788b789ceef7da
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-11-22 11:35:28 -0600

    init commit

---
+++ bangazon-financial-reporting-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bangazon_financial_reporting_tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b9d63d9-632b-4835-8119-bd268699b4e3")]
---
+++ src/bangazon-financial-reporting-tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bangazon_financial_reporting_tests
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
