0467e4fe7c083e34772edab8f54b68280c703635
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-11-22 14:34:21 -0600

    added DB connection

---
+++ src/bangazon-financial-reporting/Data/DatabaseConnection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bangazon_financial_reporting.Data
+{
+    /**
+     * Class: DatabaseConnection
+     * Purpose: Connects App with DB allows for reading SQL commands
+     * Author: Garrett Vangilder
+     * Methods:
+     *     
+     *     void execute(string query, Action<SqliteDataReader> handler) - This method opens the database, executes a given command(handler) and then closes the connection
+     */
+    public class DatabaseConnection
+    {
+        private string _path = "Data Source=" + System.Environment.GetEnvironmentVariable("REPORTING_DB_PATH");
+
+        public void execute(string query, Action<SqliteDataReader> handler)
+        {
+            SqliteConnection dbconn = new SqliteConnection(_path);
+
+            dbconn.Open();
+            SqliteCommand dbcmd = dbconn.CreateCommand();
+            dbcmd.CommandText = query;
+
+            using (var reader = dbcmd.ExecuteReader())
+            {
+                handler(reader);
+            }
+
+            dbcmd.Dispose();
+            dbconn.Close();
+        }
+    }
+}
