90a99334800830ad53e98c45ceca460f44d55018
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-11-28 10:48:48 -0600

    added more tests

---
+++ bangazon-financial-reporting-tests/UtilityTests.cs
@@ -0,0 +1,30 @@
+﻿using bangazon_financial_reporting.Factories;
+using bangazon_financial_reporting.Helpers;
+using bangazon_financial_reporting.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace bangazon_financial_reporting_tests
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void ParseDateMethodReturnsProperlyFormatedStrings()
+        {
+            string date = "20161005";
+            DateTime newDate = Utility.ParseDateMethod(date);
+            DateTime oldDate = new DateTime(2016, 10, 05);
+            Assert.Equal(newDate, oldDate);
+        }
+
+        [Fact]
+        public void GetOrdersByCustomerIdReturnsOnlyOrdersCorrespondingToTheCustomerId()
+        {
+            List<CustomerOrder> desiredOrders = Utility.GetOrdersByCustomer(1);
+            Assert.NotEmpty(desiredOrders);
+        }
+    }
+}
